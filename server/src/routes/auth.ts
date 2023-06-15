acf72e9ad8feb3abc81758cb700cd5ff6678178d
Spandan Barve <spandan567@gmail.com>
2023-06-15 15:51:31 +0530

    auth routes in server - api gateway

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,6 +3,30 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+const refreshTokens: string[] = [];
+
+router.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    {},
+    (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      const accessToken = generateAccessToken({
+        username: (user as any).name,
+      });
+      res.json({ accessToken: accessToken });
+    }
+  );
+});
+
+router.delete("/logout", (req, res) => {});
+
 router.post("/login", (req, res) => {
   const username = req.body.username;
 
@@ -10,8 +34,11 @@ router.post("/login", (req, res) => {
 
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
 
-  res.status(200).json({ accessToken: accessToken });
+  res
+    .status(200)
+    .json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
 export default router;
